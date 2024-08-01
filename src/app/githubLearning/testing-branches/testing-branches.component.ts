619da0ca30fd0017f8f6260ea04926c5397bee69
bq-bhagyashri-38 <bhagyashri.tamrakar@betaque.com>
2024-08-01 11:35:23 +0530

    testing branch code

---
+++ src/app/githubLearning/testing-branches/testing-branches.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-branches',
+  standalone: true,
+  imports: [],
+  templateUrl: './testing-branches.component.html',
+  styleUrl: './testing-branches.component.scss'
+})
+export class TestingBranchesComponent {
+
+}
