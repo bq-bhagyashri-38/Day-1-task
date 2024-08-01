169a8c692e6b5666d659148ddd2e1af158970d66
bq-bhagyashri-38 <bhagyashri.tamrakar@betaque.com>
2024-08-01 12:31:33 +0530

    testing another branch

---
+++ src/app/githubLearning/git-learn/git-learn.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-git-learn',
+  standalone: true,
+  imports: [],
+  templateUrl: './git-learn.component.html',
+  styleUrl: './git-learn.component.scss'
+})
+export class GitLearnComponent {
+
+}
